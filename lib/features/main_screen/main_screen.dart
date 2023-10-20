2b9709e0836c552b86cd30dfed8d9362c6444abc
ijonghyeon <academy3746@gmail.com>
2023-10-20 20:42:48 +0900

    1. 4th Challenge

--- lib/features/main_screen/main_screen.dart
+++ lib/features/main_screen/main_screen.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   static String routeName = "/main";
@@ -13,6 +11,26 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  List myBand = [
+    "Led Zeppelin",
+    "Deep Purple",
+    "Black Sabbath",
+    "Oasis",
+    "Korn",
+    "Judas Priest",
+    "White Snake",
+    "Queen",
+    "The Who",
+    "KISS",
+    "Nirvana",
+    "Metallica",
+    "MegaDeth",
+    "Helloween",
+    "Stratovarius",
+    "Iron Maiden",
+    "Radiohead",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,92 +48,14 @@ class _MainScreenState extends State<MainScreen> {
           vertical: 20,
           horizontal: 30,
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "I'm Administrator.",
-              style: TextStyle(
-                fontSize: 24,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Row(
-              children: [
-                Expanded(
-                  child: Text("Deep Purple"),
-                ),
-                Expanded(
-                  child: Text("Led Zeppelin"),
-                ),
-                Expanded(
-                  child: Text("Black Sabbath"),
-                ),
-              ],
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.blue,
-              ),
-              margin: const EdgeInsets.only(
-                top: 20,
-                bottom: 20,
-              ),
-              width: 300,
-              height: 80,
-              alignment: Alignment.center,
-              child: Text(
-                "Ronnie James DIO",
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.grey.shade400,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            Row(
-              children: [
-                Image.asset(
-                  "assets/images/mac.jpg",
-                  width: 100,
-                  height: 100,
-                ),
-                const FaIcon(
-                  FontAwesomeIcons.music,
-                  size: 75,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).primaryColor,
-                fixedSize: const Size(
-                  100,
-                  45,
-                ),
-                elevation: 10,
-              ),
-              onPressed: () {
-                print("ElevatedButton has pressed.");
-              },
-              child: const Text(
-                "Next",
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-          ],
+        child: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text("Hello, ${myBand[index]}!"),
+              subtitle: const Text("They are Rock Star!"),
+            );
+          },
+          itemCount: myBand.length,
         ),
       ),
     );
